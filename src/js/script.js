33dc29719be0d1a2f582af04f0378084637f1d02
jordan-liubenov <jordan.liubenov62@gmail.com>
2021-12-07 05:00:03 +0200

    added button functionality

---
+++ src/js/script.js
@@ -0,0 +1,14 @@
+function generatePassword() {
+    document.getElementById("area").value = "this is a password";
+    console.log(`${document.getElementById("area").value} generated`)
+
+}
+
+
+
+
+const allCharacters = {
+    lowerCase: ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'],
+    upperCase: ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'],
+    numbers: ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'],
+    specialChars: ['+', '-', '&', '|', '!', '(', ')', '{', '}', '[', ']', '^', '~', '*', '?', ':']};
